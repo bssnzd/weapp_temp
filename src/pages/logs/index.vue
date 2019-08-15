abd4b3c6d9582911b6124412bd9b178ad8942e88
bssnzd <1151801397@qq.com>
2019-08-15 16:41:45 +0800

    创建了模板

---
+++ src/pages/logs/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <card text='bssnzd'/>
+      <img :src="img" alt="">
+      <button @click="getimg">img</button>
+  </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+      img:''
+    }
+  },
+  methods:{
+    getimg(){
+      console.log(1)
+      this.$apis.test().then((res)=>{
+        console.log(res)
+        this.img="http://res.bestcake.com"+res.Tag.DataList[0].ImgUrl
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+img{
+  width: 206.5px;
+  height: 275px;
+}
+</style>
